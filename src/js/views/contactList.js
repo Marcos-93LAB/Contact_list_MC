f89e7ba2d0eb4ff123fe9ef2a34502d194076872
Marcos Cabral Peralta <156668187+Marcos-93LAB@users.noreply.github.com>
2024-07-03 15:51:46 +0000

    more changes

--- src/js/views/contactList.js
+++ src/js/views/contactList.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/demo.css";
 import getState from "../store/flux";
-import { CardContact } from "../component/CardContact";
+import { CardContact} from "../component/CardContact";
 
 export const ContactList = () => {
   const { store, actions } = useContext(Context);
