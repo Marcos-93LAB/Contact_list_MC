f6c7acbff71134b85e730a6fd4a7e4a1d65593fe
Marcos Cabral Peralta <156668187+Marcos-93LAB@users.noreply.github.com>
2024-07-03 15:27:30 +0000

    changes

--- src/js/layout.js
+++ src/js/layout.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
+import { ContactList } from "./views/ContactList";
+import { ContactView, Single } from "./views/ContactView";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+import { Footer } from "./component/Footer";
+import { AddContact } from "./views/AddContact";
+import { CardContact } from "./component/CardContact";
 
 //create your first component
 const Layout = () => {
@@ -16,22 +18,34 @@ const Layout = () => {
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";
 
-	return (
-		<div>
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
-						<Route path="*" element={<h1>Not found!</h1>} />
-					</Routes>
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contactlist" element={<ContactList />} />
+            <Route path="/addcontact" element={<AddContact />} />
+            <Route path="/addcontact/:id" element={<AddContact />} />
+            <Route path="/cardcontact/:id" element={<CardContact />} />
+            <Route
+              path="*"
+              element={
+                <>
+                  <h1 className="mx-auto ms-auto not-found">
+                    An error ocurred, page not found!
+                  </h1>
+                  <h5>Click on "My Contact List App" to go back Home</h5>
+                </>
+              }
+            />
+          </Routes>
+          <Footer />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
